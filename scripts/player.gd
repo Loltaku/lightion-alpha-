c11f1a00e095dcd0e7c07dc1d623363906d2f6a4
Loltaku <gongzhudianxiammd@163.com>
2025-04-03 13:45:29 +0800

    自定义按键

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,16 +6,16 @@ const JUMP_VELOCITY = -239.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# 添加重力
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# 处理跳跃
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# 获取输入方向和处理移动方向
+	# 自定义输入
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
