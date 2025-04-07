3efb70dc695c113a62815732b84bf1a0973bd120
Loltaku <gongzhudianxiammd@163.com>
2025-04-07 16:11:56 +0800

    加入加减速玩家脚本

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 100.0
+const RUN_MAX_SPEED := 96.0
+const RUN_ACCELERATION := 800.0
+const RUN_DECELERATION := 1600.0
 const JUMP_VELOCITY = -239.0
 
 
@@ -10,16 +13,27 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# 处理跳跃
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#region 跳跃
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#endregion
+	
+	#region 移动
 	# 获取输入方向和处理移动方向
-	# 自定义输入
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
+	
+	# 计算目标速度和加速度
+	var target_speed := direction * RUN_MAX_SPEED  #计算目标速度
+	var run_speed := RUN_DECELERATION  # 默认使用减速
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# 当输入方向与当前速度方向相反时保持使用DECELERATION，否则使用ACCELERATION
+		if velocity.x == 0 || sign(velocity.x) == sign(direction):
+			run_speed = RUN_ACCELERATION
+	
+	# 平滑调整速度
+	velocity.x = move_toward(velocity.x, target_speed, run_speed * delta)
+	# 执行移动
 	move_and_slide()
+	#endregion
+	
