96abfd31e66c35373988622743929ad802359e67
Loltaku <gongzhudianxiammd@163.com>
2025-04-07 17:30:25 +0800

    改成了Brackeys同款player脚本 去掉了加减速设定

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,44 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
-const RUN_MAX_SPEED := 96.0
-const RUN_ACCELERATION := 800.0
-const RUN_DECELERATION := 1600.0
+const SPEED := 96.0
 const JUMP_VELOCITY = -239.0
 
+# 将项目设置中的重力与刚体节点同步
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta):
 	# 添加重力
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	#region 跳跃
+	# 处理跳跃
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	#endregion
-	
-	#region 移动
-	# 获取输入方向和处理移动方向
+
+	# 获取输入方向:-1, 0, 1
 	var direction := Input.get_axis("left", "right")
-	
-	# 计算目标速度和加速度
-	var target_speed := direction * RUN_MAX_SPEED  #计算目标速度
-	var run_speed := RUN_DECELERATION  # 默认使用减速
+
+	# 翻转精灵图
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	# 播放动画
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+
+	# 应用移动
 	if direction:
-	
-	# 当输入方向与当前速度方向相反时保持使用DECELERATION，否则使用ACCELERATION
-		if velocity.x == 0 || sign(velocity.x) == sign(direction):
-			run_speed = RUN_ACCELERATION
-	
-	# 平滑调整速度
-	velocity.x = move_toward(velocity.x, target_speed, run_speed * delta)
-	# 执行移动
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
-	#endregion
-	
