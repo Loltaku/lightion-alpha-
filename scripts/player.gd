b5c7ab7d57a97c750bb559f9bb3d7d8f35a82ad5
Loltaku <gongzhudianxiammd@163.com>
2025-04-08 17:45:59 +0800

    加了lol下蹲

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED := 96.0
+const SPEED := 93.0
 const JUMP_VELOCITY = -239.0
 
 # 将项目设置中的重力与刚体节点同步
@@ -12,6 +12,8 @@ func _physics_process(delta):
 	# 添加重力
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+	# 土狼时间
 
 	# 处理跳跃
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -29,7 +31,9 @@ func _physics_process(delta):
 	# 播放动画
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite.play("idle")
+			if Input.is_action_pressed("lol"):
+				animated_sprite.play("lol")
+			else: animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
 	else:
