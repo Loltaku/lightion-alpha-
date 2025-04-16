60dd2c2d7204dcce48f11f258f59d48c42ed32c4
Loltaku <gongzhudianxiammd@163.com>
2025-04-16 21:15:25 +0800

    注释了一下 今晚写不完土狼了呜呜呜

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-const SPEED := 93.0
-const JUMP_VELOCITY = -239.0
+const SPEED := 93.0  # 倒过来就是 39 ( • ̀ω•́ )✧
+const JUMP_VELOCITY = -239.0  # 爱 miku
 
 # 将项目设置中的重力与刚体节点同步
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D  # 后期请替换成 AnimationTree 哦~
+@onready var coyote_timer: Timer = $CoyoteTimer  # 单次触发的timer节点
 
 func _physics_process(delta):
 	# 添加重力
@@ -17,7 +18,7 @@ func _physics_process(delta):
 	
 
 	# 处理跳跃
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():  # 如果玩家刚刚按下 “C” 并且 在地面上
 		velocity.y = JUMP_VELOCITY
 
 	# 获取输入方向:-1, 0, 1
@@ -29,6 +30,7 @@ func _physics_process(delta):
 	elif direction < 0:
 		animated_sprite.flip_h = true
 
+#region 动画控制v0.1 （if 函数直接控制 (,,>﹏<,,) ）
 	# 播放动画
 	if is_on_floor():
 		if direction == 0:
@@ -39,11 +41,12 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+#endregion
 
 	# 应用移动
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED  # 计算当前移速
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)  # 当前移速， 目标移速， 速度
 
 	move_and_slide()
