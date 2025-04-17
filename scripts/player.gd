e50f6bcb3baadc679b29f547e03aca7246c2b09e
Loltaku <gongzhudianxiammd@163.com>
2025-04-17 18:05:51 +0800

    有个bug就是如果我在0.239内按了多次c有时候会踩空气跳跃

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,36 +1,41 @@
 extends CharacterBody2D
 
-@export var SPEED := 93.0  # 倒过来就是 39 ( • ̀ω•́ )✧
-@export var JUMP_VELOCITY = -239.0  # 爱 miku
+@export var SPEED := 93.0  # 倒过来就是 39 ( • ̀ω•́ )✧ /秒
+@export var JUMP_VELOCITY = -239.0  # 爱 miku /秒
+@export var COYOTE_TIME := 0.239  # 土狼时间窗口 /秒
 
-# 将项目设置中的重力与刚体节点同步
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")  # 将项目设置中的重力与刚体节点同步s
+var last_grounded_time := 0.0  # 最后一次接触地面的时间戳 /秒
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D  # 后期请替换成 AnimationTree 哦~
 
 func _physics_process(delta):
-	# 添加重力
+# 添加重力
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# 土狼时间
-	
+# 横向移动
+	var direction := Input.get_axis("left", "right")  # 获取输入方向:-1, 0, 1
+	velocity.x = direction * SPEED if direction else move_toward(velocity.x, 0, SPEED)
 
-	# 处理跳跃
-	if Input.is_action_just_pressed("jump") and is_on_floor():  # 如果玩家刚刚按下 “C” 并且 在地面上
+# 更新移动和地面状态
+	move_and_slide()
+
+# 记录最后一次在地面的时间
+	if is_on_floor():
+		last_grounded_time = Time.get_ticks_msec() / 1000.0  # 将毫秒转换为秒
+
+# 处理跳跃
+	if Input.is_action_just_pressed("jump") and _is_on_floor():  # 如果玩家刚刚按下 “C” 并且 真！在地面上
 		velocity.y = JUMP_VELOCITY
 
-	# 获取输入方向:-1, 0, 1
-	var direction := Input.get_axis("left", "right")
-
-	# 翻转精灵图
+#region 动画控制v0.1 【if 函数直接控制】
+# 翻转精灵图
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-
-#region 动画控制v0.1 （if 函数直接控制 (,,>﹏<,,) ）
-	# 播放动画
+# 播放动画
 	if is_on_floor():
 		if direction == 0:
 			if Input.is_action_pressed("lol"):
@@ -42,10 +47,7 @@ func _physics_process(delta):
 		animated_sprite.play("jump")
 #endregion
 
-	# 应用移动
-	if direction:
-		velocity.x = direction * SPEED  # 计算当前移速
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)  # 当前移速， 目标移速， 速度
-
-	move_and_slide()
+# 真！在地面上の检测：实际在地面，或处于土狼时间窗口内
+func _is_on_floor() -> bool:
+	var current_time = Time.get_ticks_msec() / 1000.0  # 将毫秒转换为秒
+	return is_on_floor() or (current_time - last_grounded_time <= COYOTE_TIME)
